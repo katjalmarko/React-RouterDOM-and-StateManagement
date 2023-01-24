f2022e5eb89e953c6fcf6b2588b8f3b6e28018ab
katjalmarko <katjalmarko@gmail.com>
2023-01-24 16:14:04 +0100

    added custom counter hook to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,21 @@
+import "./Contact.css"
+import { useCount } from "./useCount"
+
 export const Contact = () => {
-  return <h1> THIS IS Contact PAGE </h1>
+
+  const {count, increase, decrease, reset} = useCount()
+
+  return (
+    <div className="contact">
+      <h1> THIS IS Contact PAGE </h1>
+      
+       <div className="contactButtons">
+        <button onClick={increase}>+</button>
+        <button onClick={reset}>reset</button>
+        <button onClick={decrease}>-</button>
+       </div>
+
+      <h2>{count}</h2>
+    </div>
+  )
 }
