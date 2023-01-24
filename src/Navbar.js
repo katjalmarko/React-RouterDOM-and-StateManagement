1cc281f41d408979bbb261bba15f53e8bed8f19c
katjalmarko <katjalmarko@gmail.com>
2023-01-24 17:30:59 +0100

    added a little css styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,20 @@
 import { Link } from 'react-router-dom'
+import "./Navbar.css"
+
 export const Navbar = () => {
   return (
-    <div>  
-      <Link to="/" > Home </Link> 
-      <Link to="/menu" > Profile </Link> 
-      <Link to="/contact" > Contact </Link> 
+    <div className='Navbar'>  
+      <div className='HomeNavbar'>
+        <Link to="/" > Home </Link> 
+      </div>
+
+      <div className='ProfileNavbar'>
+        <Link to="/menu" > Profile </Link> 
+      </div>
+
+      <div className='ContactNavbar'> 
+        <Link to="/contact" > Contact </Link>
+      </div>  
     </div>
   )
 }
